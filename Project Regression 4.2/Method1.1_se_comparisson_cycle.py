9729fc639d5decefd9a49dea95d92b05417b1aa1
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-10-14 20:15:57 +0100

    SE Ciclico

--- Project Regression 4.2/Method1.1_se_comparisson_cycle.py
+++ Project Regression 4.2/Method1.1_se_comparisson_cycle.py
@@ -8,10 +8,10 @@ print("Determining outliars using ciclical SE comparisson method")
 
 Test_passed = False
 out_list = []
-
 x_import_wo, y_import_wo = x_import.copy(), y_import.copy()
 
-while not Test_passed:
+
+while not Test_passed and len(out_list) < 20:
 
     modelwout = linear_model(x_import_wo,y_import_wo)
     y_pred = modelwout.predict(x_import_wo)
@@ -20,61 +20,18 @@ while not Test_passed:
     for i in range(len(x_import_wo)):
         SE_list = np.hstack([SE_list,(y_import_wo[i]-y_pred[i])**2])
     
-    sigma = np.std(SE_list)
-    threshold = np.mean(SE_list) + 0.1*sigma
-
+    threshold = np.mean(SE_list) + 3.7* np.std(SE_list)
     outlier = np.amax(SE_list)
     outlier_index = np.where(SE_list == outlier)[0][0]
+
  
     if SE_list[outlier_index] > threshold:
         print('Outlier removed:',outlier_index)
-        out_list.append(np.where(x_import == x_import_wo[outlier_index]))
-        np.delete(x_import_wo,[outlier_index],axis=0)
-        np.delete(y_import_wo,[outlier_index],axis=0)
+        out_list.append(np.where(x_import == x_import_wo[outlier_index])[0][0])
+        x_import_wo = np.delete(x_import_wo,outlier_index,axis=0)
+        y_import_wo = np.delete(y_import_wo,outlier_index,axis=0)
 
     else:
-        Test_passed == True
+        Test_passed = True
 
-
-
-
-
-    
-
-
-
-
-modelwout = linear_model(x_import,y_import)
-SE_list, out_list = [], []
-
-y_pred = modelwout.predict(x_import)
-
-for i in range(len(x_import)):
-    SE_list.append((y_import[i]-y_pred[i])**2)
-
-sigma = np.std(SE_list)
-threshold = np.mean(SE_list) + 0.1*sigma
-
-
-plt.plot(index_list, SE_list, color='blue', marker='o')
-plt.hlines(threshold,index_list[0],index_list[-1], color='red')
-plt.title('Squared Error vs Data Index', fontsize=14)
-plt.ylabel('SE', fontsize=14)
-plt.xlabel('Data Index', fontsize=14)
-plt.grid(True)
-# plt.show()
-
-
-for i in range(len(x_import)):
-    if SE_list[i] > threshold:
-        out_list.append(i)
-
-print('There are',len(out_list),' outliers :\n',out_list)
-
-x_import_wo = np.delete(x_import, out_list, axis=0)
-y_import_wo = np.delete(y_import, out_list, axis=0)
-
-
-alpha_list = np.linspace(0.001,2,50)
-determine_best_model(x_import_wo,y_import_wo,100,alpha_list)
-# It also works with just this and much faster
+print(len(out_list),'outliers found:\n',out_list)
