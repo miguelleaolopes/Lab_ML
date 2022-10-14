01d22caa1bec8409b659c7748e1887abe7dd3905
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-10-14 20:49:53 +0100

    completing the SE cyclic

--- Project Regression 4.2/Method1.1_se_comparisson_cycle.py
+++ Project Regression 4.2/Method1.1_se_comparisson_cycle.py
@@ -20,7 +20,7 @@ while not Test_passed and len(out_list) < 20:
     for i in range(len(x_import_wo)):
         SE_list = np.hstack([SE_list,(y_import_wo[i]-y_pred[i])**2])
     
-    threshold = np.mean(SE_list) + 3.7* np.std(SE_list)
+    threshold = np.mean(SE_list) + 4.3*np.std(SE_list)
     outlier = np.amax(SE_list)
     outlier_index = np.where(SE_list == outlier)[0][0]
 
@@ -30,8 +30,10 @@ while not Test_passed and len(out_list) < 20:
         out_list.append(np.where(x_import == x_import_wo[outlier_index])[0][0])
         x_import_wo = np.delete(x_import_wo,outlier_index,axis=0)
         y_import_wo = np.delete(y_import_wo,outlier_index,axis=0)
-
     else:
         Test_passed = True
 
+out_list = np.sort(out_list)
 print(len(out_list),'outliers found:\n',out_list)
+
+determine_best_model(x_import_wo,y_import_wo,500,np.linspace(0.001,2,50))
