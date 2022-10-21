f4fd5a0ea25cee502ab457d33953011c9b993158
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-10-21 12:18:26 +0100

    fucking commit

--- Project Regression 4.3/image_reconstruction.py
+++ Project Regression 4.3/image_reconstruction.py
@@ -8,16 +8,6 @@ y_train = np.load('data/Ytrain_Classification1.npy')
 from colorsys import hsv_to_rgb
 from PIL import Image
 
-# Make some RGB values. 
-# Cycle through hue vertically & saturation horizontally
-colors = []
-for hue in range(360):
-    for sat in range(100):
-        # Convert color from HSV to RGB
-        rgb = hsv_to_rgb(hue/360, sat/100, 1)
-        rgb = [int(0.5 + 255*u) for u in rgb]
-        colors.extend(rgb)
-
 
 # Convert list to bytes ddd
 img = Image.frombytes("RGB", (30, 30), x_train[2])
