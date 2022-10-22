e43ebb4b5ba8cc5e8f799bb9f700c58f709930d1
miguelleaolopes <miguelleaolopes@gmail.com>
2022-10-22 17:33:27 +0100

    show image now with reshape (comment previous method)

--- Project Regression 4.3/image_reconstruction.py
+++ Project Regression 4.3/image_reconstruction.py
@@ -1,15 +1,24 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 x_train = np.load('data/Xtrain_Classification1.npy')
 y_train = np.load('data/Ytrain_Classification1.npy')
 
 # reconstrução de uma das imagens sendo que os dados são listas de valores RBG de uma imagem 30x30
+x_train_mod = np.reshape(x_train,(8273,30,30,3))
+print(np.shape(x_train))
+print(np.shape(x_train_mod))
 
-from colorsys import hsv_to_rgb
-from PIL import Image
+for i in range(1,5):
+    print("Showing image {}, which is qualified as {}".format(i, y_train[i]))
+    plt.imshow(x_train_mod[i]);
+    plt.show()
 
+# from colorsys import hsv_to_rgb
+# from PIL import Image
 
-# Convert list to bytes ddd
-img = Image.frombytes("RGB", (30, 30), x_train[2])
-img.show()
-img.save('pic1.png')
+# # Convert list to bytes ddd
+# for i in range(1,5):
+#     img = Image.frombytes("RGB", (30, 30), x_train[i])
+#     img.show()
+#     img.save('pic1.png')
