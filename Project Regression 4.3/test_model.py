4b2c7d52a49fde385196671219a698c0ce95373a
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-10-26 00:39:49 +0100

    some corrections

--- Project Regression 4.3/test_model.py
+++ Project Regression 4.3/test_model.py
@@ -1,8 +1,9 @@
 from initialization import *
 from model1 import *
+from model2 import *
 
 
-m1 = model1
+m1 = model2()
 m1.create_model()
 print('\n\n\n##### End of tensorflow rant ##########\n\n\n')
 m1.summary()
