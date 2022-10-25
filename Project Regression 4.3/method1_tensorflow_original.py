01ae2e656fb220b98dd89b5ac74cc2efeba92109
miguelleaolopes <miguelleaolopes@gmail.com>
2022-10-25 19:39:43 +0100

    its quicker faster the method1_tensor

--- Project Regression 4.3/method1_tensorflow_original.py
+++ Project Regression 4.3/method1_tensorflow_original.py
@@ -25,7 +25,12 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 
 # Augmentation of data by introducing some random rotation
 
-data_augmentation = keras.Sequential()
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+    ]
+)
 
 
 def make_model(input_shape, num_classes):
@@ -35,33 +40,39 @@ def make_model(input_shape, num_classes):
 
     # Entry block
     x = layers.Rescaling(1.0 / 255)(x)
-
-    x = layers.Conv2D(32, 3, strides=2, activation='relu', padding="same")(x)
-    x = layers.MaxPooling2D((2, 2), padding="same")(x)
-    x = layers.Dropout(0.2)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
 
-    x = layers.Conv2D(64, 3, strides=2, activation='relu', padding="same")(x)
-    x = layers.MaxPooling2D((2, 2), padding="same")(x)
-    x = layers.Dropout(0.2)(x)
+    x = layers.Conv2D(64, 3, padding="same")(x)
     x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
 
-    x = layers.Conv2D(128, 3, strides=2, activation='relu', padding="same")(x)
-    x = layers.Dropout(0.2)(x)
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
     x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
 
-    x = layers.Flatten()(x)
-
-    # x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-    x = layers.Dense(256, activation='relu')(x)
-    x = layers.Dropout(0.2)(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Dense(128, activation='relu')(x)
-    x = layers.Dropout(0.2)(x)
-    x = layers.BatchNormalization()(x)
-
+    x = layers.GlobalAveragePooling2D()(x)
     if num_classes == 2:
         activation = "sigmoid"
         units = 1
@@ -69,6 +80,7 @@ def make_model(input_shape, num_classes):
         activation = "softmax"
         units = num_classes
 
+    x = layers.Dropout(0.5)(x)
     outputs = layers.Dense(units, activation=activation)(x)
     return keras.Model(inputs, outputs)
 
@@ -81,33 +93,23 @@ x = layers.Rescaling(1./255)(x)
 train_ds = train_ds.prefetch(buffer_size=32)
 val_ds = val_ds.prefetch(buffer_size=32)
 
+
+
 # Run the model
 
 model = make_model(input_shape=image_size + (3,), num_classes=2)
 keras.utils.plot_model(model, show_shapes=True)
-print(model.summary())
+
 epochs = 50
 
 callbacks = [
-    keras.callbacks.ModelCheckpoint("save_epoch_1/save_at_{epoch}.h5"),
+    keras.callbacks.ModelCheckpoint("save_at_i_h5/save_at_{epoch}.h5"),
 ]
 model.compile(
     optimizer=keras.optimizers.Adam(1e-3),
     loss="binary_crossentropy",
-    # loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
-history = model.fit(
+model.fit(
     train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
 )
-
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
-plt.savefig("method_1_acc_epo.png")
-plt.show()
-# test_loss, test_acc = model.evaluate(x_import,  y_import, verbose=2)
-# print(test_acc)
