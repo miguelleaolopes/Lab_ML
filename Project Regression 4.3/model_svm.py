9b3c02711644f30ed7192d04df001a5329b1eb24
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-10-29 00:24:41 +0100

    svm implementation

--- Project Regression 4.3/model_svm.py
+++ Project Regression 4.3/model_svm.py
@@ -12,10 +12,10 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
 
-class linear_svm:
+class svm_model:
 
-    def __init__(self):
-        self.model = svm.SVC(kernel='linear')
+    def __init__(self,kernel,gamma='auto'):
+        self.model = svm.SVC(kernel=kernel,gamma=gamma)
     
 
     def fit(self):
@@ -24,15 +24,25 @@ class linear_svm:
     def predict(self):
         self.y_pred = self.model.predict(x_test)
 
-    def calculate_score(self):
+    def calculate_metrics(self):
         self.acc = metrics.accuracy_score(y_test, self.y_pred)
+        self.f1 = metrics.f1_score(y_test, self.y_pred)
+        
 
+gammas = np.linspace(0.0001,.01,5)
 
-mod = linear_svm()
-mod.fit()
-mod.predict()
-mod.calculate_score()
-print(mod.acc)
+for gamma  in gammas:
+    # model = svm_model(kernel='poly')
+    # model = svm_model(kernel='linear')
+    model = svm_model(kernel='rbf',gamma=gamma)
+
+    model.fit()
+    model.predict()
+    model.calculate_metrics()
+
+    print('=== Gamma:', gamma)
+    print('=== Score F1:',model.f1)
+    print('== Score ACC:',model.acc)
 
 
     
