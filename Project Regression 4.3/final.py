6b09b8c2d60ded0adabb6a1c939e6bba0db2d6ef
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-10-29 21:06:43 +0100

    compare two files

--- Project Regression 4.3/final.py
+++ Project Regression 4.3/final.py
@@ -1,15 +1,15 @@
 from initialization import *
 
 my_callbacks = [
-    tf.keras.callbacks.CSVLogger("run/training.log", separator=",", append=False),
-    tf.keras.callbacks.EarlyStopping(patience=50,verbose=1,monitor='val_loss'), #change to 50 in the end 
-    tf.keras.callbacks.ModelCheckpoint(filepath='run/model_best.h5', monitor='val_accuracy', mode='max', save_best_only=True)
+    #tf.keras.callbacks.CSVLogger("run/training.log", separator=",", append=False),
+    #tf.keras.callbacks.EarlyStopping(patience=50,verbose=1,monitor='val_loss'), #change to 50 in the end 
+    tf.keras.callbacks.ModelCheckpoint(filepath='run/model_best.h5', monitor='f1_score', mode='max', save_best_only=True)
 ]
 
 
 model = models.Sequential()
 
-# With Dropout 2
+# With Test 1
 model.add(layers.Conv2D(32, (3, 3), strides=1, activation='relu', padding="same",input_shape=(30, 30, 3)))
 model.add(layers.MaxPooling2D((2, 2), padding="same"))
 model.add(layers.Dropout(0.2))
@@ -25,10 +25,11 @@ model.add(layers.Flatten())
 model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dropout(0.2))
 model.add(layers.BatchNormalization())
-model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dropout(0.2))
 model.add(layers.BatchNormalization())
 model.add(layers.Dense(1, activation="sigmoid"))
+
 
 print('\n\n\n##### End of tensorflow rant ##########\n\n\n')
 
@@ -51,7 +52,8 @@ history = model.fit(
                     train_datagen.flow(x_import, y_import), 
                     epochs=50, 
                     #validation_data=(x_test, y_test),             
-                    callbacks=my_callbacks)
+                    callbacks=my_callbacks
+                    )
 
 print('##### Model Created #####')
 model_loc = './run/model_best.h5'
@@ -65,58 +67,6 @@ y_pred_bin = np.reshape(np.rint(y_pred),(np.shape(y_pred)[0]))
 np.save('data/y_pred.npy', y_pred_bin)
 print ('Y prediction saved')
 
-
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.45, 1])
-plt.legend(loc='lower right')
-plt.savefig("models_acc_epo/model_acc_final.png")
-plt.show()
-
-plt.plot(history.history['loss'], label='loss')
-plt.plot(history.history['val_loss'], label = 'val_loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.ylim([0.0, plt.ylim()[1]])
-plt.legend(loc='lower right')
-plt.savefig("models_loss_epo/model_loss_final.png")
-plt.show()
-
-plt.plot(history.history['val_f1_score'], label = 'val_f1_score')
-plt.plot(history.history['f1_score'], label='f1_score')
-plt.xlabel('Epoch')
-plt.ylabel('F1 Score')
-plt.ylim([0.0, plt.ylim()[1]])
-plt.legend(loc='lower right')
-plt.savefig("models_f1score_epo/model_f1score_final.png")
-plt.show()
-
-plt.subplot(1, 3, 1)
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.45, 1])
-plt.legend(loc='lower right')
-plt.subplot(1, 3, 2)
-plt.plot(history.history['loss'], label='loss')
-plt.plot(history.history['val_loss'], label = 'val_loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.ylim([0.0, plt.ylim()[1]])
-plt.legend(loc='lower right')
-plt.subplots_adjust(left=0.06,bottom=0.1,right=0.94,top=0.94,wspace=0.16,hspace=0.2)
-plt.subplot(1, 3, 3)
-plt.plot(history.history['f1_score'], label='f1_score')
-plt.plot(history.history['val_f1_score'], label = 'val_f1_score')
-plt.xlabel('Epoch')
-plt.ylabel('F1 Score')
-plt.ylim([0.0, plt.ylim()[1]])
-plt.legend(loc='lower right')
-plt.subplots_adjust(left=0.06,bottom=0.1,right=0.94,top=0.94,wspace=0.16,hspace=0.2)
-plt.show()
 
 
 
