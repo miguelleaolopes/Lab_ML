5305466d661717e4a8e5f76de41d13c71674ead8
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-11-04 12:12:07 +0000

    bagging corrections

--- Project Regression 4.4/model_bagging.py
+++ Project Regression 4.4/model_bagging.py
@@ -4,22 +4,24 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import cross_val_score, cross_validate, RepeatedStratifiedKFold, KFold
 from sklearn.ensemble import BaggingClassifier
 from imblearn.ensemble import BalancedBaggingClassifier
+from sklearn.metrics import cohen_kappa_score, make_scorer
+kappa_scorer = make_scorer(cohen_kappa_score)
 
 #Import libraries
 x_import = np.load('data/Xtrain_Classification2.npy')/255.0
 y_import = np.load('data/Ytrain_Classification2.npy')
 
 
-model = BaggingClassifier()
-# model = BalancedBaggingClassifier()
+# model = BaggingClassifier()
+model = BalancedBaggingClassifier()
 
 # define evaluation procedure
 # cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1) #RepeatedStratifiedKfold does not support multiclassification
 cv = KFold(n_splits=20)
 
 
-# evaluate model
-scores = cross_val_score(model, x_import, y_import, scoring='accuracy', cv=cv, n_jobs=-1) #metrics.balanced_accuracy_score
 
+# evaluate model, cross_validate instead is necessary for multiple scoring
+scores = cross_validate(model, x_import, y_import , scoring={'accuracy':'accuracy','kappa':kappa_scorer}, cv=cv, n_jobs=-1)
 # summarize performance
-print('Mean Accuracy: %.3f' % mean(scores))
+print('Mean Accuracy: {}\n Mean Kappa: {}'.format(mean(scores['test_accuracy']),mean(scores['test_kappa'])))
