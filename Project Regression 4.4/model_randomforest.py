9839cf8f6f06abfe891ac2bec205f721f409c618
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-11-04 10:16:38 +0000

    implementation of cohen's kappa in random forest

--- Project Regression 4.4/model_randomforest.py
+++ Project Regression 4.4/model_randomforest.py
@@ -2,9 +2,13 @@
 import numpy as np
 from numpy import mean
 from sklearn.datasets import make_classification
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.model_selection import RepeatedStratifiedKFold, KFold
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import cohen_kappa_score, make_scorer
+
+kappa_scorer = make_scorer(cohen_kappa_score)
+
 
 #Import libraries
 x_import = np.load('data/Xtrain_Classification2.npy')/255.0
@@ -16,7 +20,7 @@ model = RandomForestClassifier(n_estimators=10, class_weight='balanced')
 # cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 cv = KFold(n_splits=20)
 
-# evaluate model
-scores = cross_val_score(model, x_import, y_import , scoring='accuracy', cv=cv, n_jobs=-1)
+# evaluate model, cross_validate instead is necessary for multiple scoring
+scores = cross_validate(model, x_import, y_import , scoring={'accuracy':'accuracy','kappa':kappa_scorer}, cv=cv, n_jobs=-1)
 # summarize performance
-print('Mean Accuracy: %.3f' % mean(scores))
+print('Mean Accuracy: {}\n Mean Kappa: {}'.format(mean(scores['test_accuracy']),mean(scores['test_kappa'])))
