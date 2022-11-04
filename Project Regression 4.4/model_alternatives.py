4e21e238a3b0be8ef0883a904fe6d1d25536403d
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-11-04 13:34:02 +0000

    More corrections to model_alternatives

--- Project Regression 4.4/model_alternatives.py
+++ Project Regression 4.4/model_alternatives.py
@@ -3,7 +3,7 @@ from numpy import mean
 from sklearn.datasets import make_classification
 from sklearn.model_selection import cross_val_score, cross_validate, RepeatedStratifiedKFold, KFold
 from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
-from imblearn.ensemble import BalancedBaggingClassifier
+from imblearn.ensemble import BalancedBaggingClassifier, BalancedRandomForestClassifier
 from sklearn.metrics import cohen_kappa_score, make_scorer, balanced_accuracy_score
 
 
@@ -17,15 +17,28 @@ y_import = np.load('data/Ytrain_Classification2.npy')
 
 class alternative_model:
 
-    def __init__(self, model_type='RandomForest', n_splits = 10):
+    def __init__(self, model_type='RandomForest', n_splits = 10, class_weight='balanced'):
+        '''
+        model_type -> Specifies the model we want to use
+        n_splits -> Necessary for all
+        class_weights -> Necesary for Random Forest ['balanced', 'balanced_subsample']
+        '''
+
+
+
         self.model_type = model_type
-        print('Created {} model with {} splits'.format(model_type, n_splits))
+        print('Created {} model with {} n_splits'.format(model_type, n_splits))
         if model_type == 'Balanced Bagging':
             self.model = BalancedBaggingClassifier()
+        
         elif model_type == 'Bagging':
             self.model = BaggingClassifier()
-        elif model_type == 'RandomForest':
-            self.model = RandomForestClassifier(n_estimators=10, class_weight='balanced')
+        
+        elif model_type == 'Random Forest':
+            self.model = RandomForestClassifier(n_estimators=10, class_weight=class_weight)
+
+        elif model_type == 'Balanced Random Forest':
+            self.model = BalancedRandomForestClassifier(n_estimators=10, class_weight=class_weight)
         
         self.n_splits = n_splits 
 
@@ -47,27 +60,39 @@ class alternative_model:
             print('CV type not supported')
             quit()
 
-        print('\n\n {} Results ======'.format(self.mode_type))
-        print('\n Mean Accuracy {}\n Mean Balenced Accuracy: {}\n Mean Kappa: {}'.format(mean(self.scores['test_accuracy']),mean(self.scores['test_baccuracy']),mean(self.scores['test_kappa'])))
+        print('\n\n {} Results ======'.format(self.model_type))
+        print('\n Mean Accuracy {}\n Mean Balenced Accuracy: {}\n Mean Kappa: {}\n\n\n'.format(mean(self.scores['test_accuracy']),mean(self.scores['test_baccuracy']),mean(self.scores['test_kappa'])))
 
 
-modelRF = alternative_model(
-    model_type = 'RandomForest',
-    n_splits = 15
-    )
+
+
+
+
+# modelB = alternative_model(
+#     model_type = 'Bagging',
+#     n_splits = 15
+# )
 
 modelBB = alternative_model(
     model_type ='Balanced Bagging',
     n_splits = 15
     )
 
-modelB = alternative_model(
-    model_type = 'Bagging',
-    n_splits = 15
+# modelRF = alternative_model(
+#     model_type = 'Random Forest',
+#     n_splits = 15,
+#     class_weight='balanced'
+#     )
+
+modelBRF = alternative_model(
+    model_type = 'Balanced Random Forest',
+    n_splits = 15,
+    class_weight='balanced'
 )
 
 
 
-modelRF.test_model()
+# modelB.test_model()
 modelBB.test_model()
-modelB.test_model()
+#modelRF.test_model()
+modelBRF.test_model()
