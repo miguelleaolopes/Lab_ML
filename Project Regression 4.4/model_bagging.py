4465397a7afa01d080dfddaf2f521a0661507fba
ricardoedgarsilva <ricardoedgarsilva@gmail.com>
2022-11-04 10:08:25 +0000

    correction of random forest implementation of balanced bagging

--- Project Regression 4.4/model_bagging.py
+++ Project Regression 4.4/model_bagging.py
@@ -4,15 +4,14 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import cross_val_score, cross_validate, RepeatedStratifiedKFold, KFold
 from sklearn.ensemble import BaggingClassifier
 from imblearn.ensemble import BalancedBaggingClassifier
-from keras.utils import np_utils
 
 #Import libraries
 x_import = np.load('data/Xtrain_Classification2.npy')/255.0
 y_import = np.load('data/Ytrain_Classification2.npy')
 
 
-# model = BaggingClassifier()
-model = BalancedBaggingClassifier()
+model = BaggingClassifier()
+# model = BalancedBaggingClassifier()
 
 # define evaluation procedure
 # cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1) #RepeatedStratifiedKfold does not support multiclassification
@@ -20,7 +19,7 @@ cv = KFold(n_splits=20)
 
 
 # evaluate model
-scores = cross_validate(model, x_import, y_import, scoring='accuracy', cv=cv, n_jobs=-1) #metrics.balanced_accuracy_score
+scores = cross_val_score(model, x_import, y_import, scoring='accuracy', cv=cv, n_jobs=-1) #metrics.balanced_accuracy_score
 
 # summarize performance
-print('Mean Accuracy: %.3f' % mean(scores['test_score']))
+print('Mean Accuracy: %.3f' % mean(scores))
--- Project Regression 4.4/model_randomforest.py
+++ Project Regression 4.4/model_randomforest.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import mean
 from sklearn.datasets import make_classification
 from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import RepeatedStratifiedKFold, KFold
 from sklearn.ensemble import RandomForestClassifier
 
 #Import libraries
@@ -13,8 +13,10 @@ y_import = np.load('data/Ytrain_Classification2.npy')
 # define model
 model = RandomForestClassifier(n_estimators=10, class_weight='balanced')
 # define evaluation procedure
-cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+cv = KFold(n_splits=20)
+
 # evaluate model
-scores = cross_val_score(model, x_import, y_import , scoring='roc_auc', cv=cv, n_jobs=-1)
+scores = cross_val_score(model, x_import, y_import , scoring='accuracy', cv=cv, n_jobs=-1)
 # summarize performance
-print('Mean ROC AUC: %.3f' % mean(scores))
+print('Mean Accuracy: %.3f' % mean(scores))
