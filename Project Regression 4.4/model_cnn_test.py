cc9c6b3ec04d3427f7f6132db2fff2294edd1bb5
miguelleaolopes <miguelleaolopes@gmail.com>
2022-11-04 21:43:22 +0000

    fuck keras

--- Project Regression 4.4/model_cnn_test.py
+++ Project Regression 4.4/model_cnn_test.py
@@ -13,16 +13,16 @@ model_used = "LeNet"
 model_used = "U_Net"
 model_used = "new_4"
 model_used = "new_3"
-model_used = "new_2"
-model_used = "new_1"
 model_used = "task3_model_dropout"
 model_used = "task3_model"
+model_used = "new_1"
+model_used = "new_2"
 
 m2 = model(data_augmentation=False) #data_augmentation=data_augmentations
 m2.layers(model_used)
 print('\n\n\n##### End of tensorflow rant ##########\n\n\n')
 m2.summary()
 m2.compile(epoch=10,calls=my_callbacks,compiler = "adam_bal")  #adam_bin
-m2.show_acc_plt(model_used,save_img = True)
+m2.show_acc_plt(model_used,save_img = False)
 m2.show_acc_val()
 
